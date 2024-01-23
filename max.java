0a3955166a1f172969183830c7b7f2efea978559
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-01-23 16:36:58 +0300

    Create max.java

---
+++ max.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class NaolMain{
+  public static int max(int i,int j){
+    return Math.max(i,j);
+  }
+  public static void main(String[] args){
+    System.out.println("Hello from the other side");
+    System.out.println(max(2,4));
+  }
+}
